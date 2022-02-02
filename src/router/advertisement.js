f56bfd54011b6a464a026e990a9ffb020f8d006b
Elturayev_Dilshod <88940687+elturayev@users.noreply.github.com>
2022-02-03 02:48:03 +0500

    Update advertisement.js

--- src/router/advertisement.js
+++ src/router/advertisement.js
@@ -3,9 +3,8 @@ const controller = require('../controllers/advertisement.js')
 const validator = require('../middlewares/validation.js')
 
 router.get('/',controller.GET)
-router.get('/:pendding',controller.GET)
+router.get('/:adminAd',controller.GET)
 router.get('/active/:id',controller.ACTIVE)
 router.post('/',validator.validAd,controller.POST)
 router.put('/',controller.PUT)
-router.delete('/cancel',controller.DELETE)
 module.exports = router
