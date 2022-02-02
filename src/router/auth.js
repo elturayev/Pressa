6cb26497ed9d9571c0d541b27b3da0905451c073
Elturayev_Dilshod <88940687+elturayev@users.noreply.github.com>
2022-02-03 02:49:01 +0500

    Update auth.js

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const controller = require('../controllers/auth.js')
 const validator = require('../middlewares/validation.js')
+router.get('/valid',validator.valid,controller.VALID)
 router.post('/login',validator.validLogin,controller.LOGIN)
 
 module.exports = router
