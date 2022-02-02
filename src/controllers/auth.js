15324be5b90806a7fc3c00dd06cd762fad348544
Elturayev_Dilshod <88940687+elturayev@users.noreply.github.com>
2022-02-03 02:43:48 +0500

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 const sha256 = require('sha256')
 const { ClientError } = require('../utils/error.js')
+const jwt = require('jsonwebtoken')
 
 const LOGIN = (request,response,next)=>{
 	try{
@@ -8,8 +9,9 @@ const LOGIN = (request,response,next)=>{
 		let admin = users[0]
 		if(admin.name == username && admin.password == sha256(password)){
 			response.json({
-				status: 555,
-				message: "The admin is successfully on logged!"
+				status: 200,
+				message: "The admin is successfully on logged!",
+				token: jwt.sign({id: admin.user_id},'secret_key')
 			})
 		}else  throw new ClientError(401,"The admin is password and username error entered!")
 		return next()
@@ -18,6 +20,18 @@ const LOGIN = (request,response,next)=>{
 	}
 }
 
+const VALID = (request,response,next)=>{
+	try{
+		response.json({
+			status:200,
+			message: 'Token valid!'
+		})
+	}catch(error){
+		return next(error)
+	}
+}
+
 module.exports = {
 	LOGIN,
+	VALID
 }
